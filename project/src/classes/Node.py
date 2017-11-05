2cfc4f83232143a047a1cc3b3b8dee0125935e8e
Paulo Gouveia <paulojlgouveia@gmail.com>
2017-11-05 23:14:56 +0000

    regex; sort of creating the execution graph (I am failing with the string regex)

--- project/src/classes/Node.py
+++ project/src/classes/Node.py
@@ -1,29 +1,52 @@
 
+import re
+
+
 class Node(object):
 	
-	def __init__(self, type = "???", name = "function", code = ""):
+	def __init__(self, type = "???", name = "function", code = None):
 		self.type = type
 		self.name = name
 		
-		arguments = []
+		self.arguments = []
 		
-		code = code.split(';\n')
-		for line in code:
-			line = line.replace('\n', '').split('=', 1)
-			
-			if line[0].startswith('$'):
-				newNode = Node.Node("variable", line[0], line[1])
+		if code != "" and code != None:
+			code = code.split(';\n')
+			for line in code:
+				newNode = None
 				
-			else:
-				l = line[0].index('(')
-				r = line[0].index(')')
-				newNode = Node.Node("function", line[0], line[0][l:r])
-			
-			arguments.append(newNode)
-		
-		self.arguments = arguments
+				
+				assignment = re.search(r'(\$\w+)=(\S+)', line)
+				function = re.search(r'(\w+)\((\S+)\)', line)
+				variable = re.search(r'(\$\S+)', line)
+				string = re.search(r"['\"](.*?)['\"]", line)
+				#string = re.search(r'(.*)', line)
+				
+				
+				if assignment:
+					print("a", assignment.group(1),assignment.group(2))
+					newNode = Node("variable", assignment.group(1), assignment.group(2))
+					
+				elif function:
+					print("f", function.group(1), function.group(2))
+					newNode = Node("function", function.group(1), function.group(2))
+					
+				elif variable:
+					print("v", variable.group(1))
+					newNode = Node("variable", variable.group(1))
+					
+				elif string:
+					# FIXME find variables
+					print("s", string.group(0))
+					newNode = string.group(0)
+					
+				else:
+					print("FAIL")
+				
+				if newNode:
+					self.arguments.append(newNode)
 	
-	
+
 	def __str__(self):
 		out = "Type:\t\t" + self.type + "\n"
 		out += "Name:\t\t" + self.name + "\n"
@@ -45,4 +68,38 @@ class Node(object):
 			
 		return self.arguments
 	
-	
+'''
+	def __init__(self, type = "???", name = "function", code = None):
+		self.type = type
+		self.name = name
+		
+		arguments = []
+		
+		if code:
+			code = code.split(';\n')
+			for line in code:
+				line = line.replace('\n', '').split('=', 1)
+				
+				if line[0].startswith('$'):
+					if len(line) > 1:
+						newNode = Node("variable", line[0], line[1])
+					else:
+						newNode = Node("variable", line[0])
+					
+				else:
+					match = re.search(r'(\w+)\( (\w+)\)', line[0])
+					
+					if match:
+						
+						l = line[0].index('(')
+						r = line[0].index(')')
+						newNode = Node("function", match.group(1), match.group(2))
+						
+						#l = line[0].index('(')
+						#r = line[0].index(')')
+						#newNode = Node("function", line[0], line[0][l:r])
+						
+				arguments.append(newNode)
+			
+		self.arguments = arguments
+'''
